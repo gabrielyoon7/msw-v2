1c74a98306a9ac86eccb11d007ff0a622f5db371
gabrielyoon7 <gabrielyoon7@gmail.com>
2023-11-10 02:39:43 +0900

    chore: tanstack query v5 설치

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
+import {ReactQueryDevtools} from "@tanstack/react-query-devtools";
 
 async function deferRender() {
   if (process.env.NODE_ENV !== 'development') {
@@ -12,11 +14,14 @@ async function deferRender() {
 
   return worker.start()
 }
-
+const queryClient = new QueryClient()
 deferRender().then(() => {
   ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
-      <App />
+      <QueryClientProvider client={queryClient}>
+        <App />
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
     </React.StrictMode>,
   )
 })
