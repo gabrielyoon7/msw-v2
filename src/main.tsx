5824857aea5d9e243fb68afff8fa2d900c717f03
gabrielyoon7 <gabrielyoon7@gmail.com>
2023-11-13 22:56:53 +0900

    chore: react-router-dom 설치

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import App from './App.tsx'
 import './index.css'
 import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
 import {ReactQueryDevtools} from "@tanstack/react-query-devtools";
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
 
 async function deferRender() {
   if (process.env.NODE_ENV !== 'development') {
@@ -15,11 +16,22 @@ async function deferRender() {
   return worker.start()
 }
 const queryClient = new QueryClient()
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+],{
+  basename: "/msw-v2/",
+});
+
 deferRender().then(() => {
   ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
       <QueryClientProvider client={queryClient}>
-        <App />
+        <RouterProvider router={router} />
         <ReactQueryDevtools initialIsOpen={false} />
       </QueryClientProvider>
     </React.StrictMode>,
